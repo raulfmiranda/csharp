a2b312a2549c6390a9676f223ec3aee964f32d56
Raul Figueira <raulfmiranda@gmail.com>
2017-05-13 10:39:55 -0300

    Feito o exercício 8

---
+++ exerc8/exerc8/exerc8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 6;
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exerc9/exerc9/exerc9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {3, 2, 3, 4, 2, 2, 4};
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" " + array[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
