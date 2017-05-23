5eaf87defc91739f28cee340476be38cfc860938
Raul Figueira <raulfmiranda@gmail.com>
2017-05-23 20:30:35 -0300

    Métodos soma, produto, mínimo, máximo e média criados

--- exerci17/exerc17/Program.cs
+++ exerci17/exerc17/Program.cs
@@ -19,10 +19,13 @@ namespace exerc17
     {
         static void Main(string[] args)
         {
-            List<double> ints = new List<double>();
-            ints.Add(1.1);
-            ints.Add(2.3);
-            Console.WriteLine(ints.Soma());
+            List<double> nums = new List<double>();
+            nums.Add(1.1);
+            nums.Add(2.3);
+            nums.Add(4.1);
+            nums.Add(2.5);
+            Console.WriteLine(" Soma: {0}\n Produto: {1}", nums.Soma(), nums.Produto());
+            Console.WriteLine(" Mínimo: {0}\n Máximo: {1}\n Média: {2}", nums.Minimo(), nums.Maximo(), nums.Media());
 
             Console.ReadLine();
         }
@@ -41,12 +44,56 @@ namespace exerc17
             return (T)Convert.ChangeType(soma, typeof(T));
         }
 
+        public static T Produto<T>(this IEnumerable<T> nums)
+        {
+            double prod = 1.0;
+
+            foreach (T num in nums)
+            {
+                prod *= (double)Convert.ChangeType(num, typeof(double));
+            }
+            return (T)Convert.ChangeType(prod, typeof(T));
+        }
+
+        public static T Minimo<T>(this IEnumerable<T> nums)
+        {
+            double minimo = (double)Convert.ChangeType(nums.ElementAt(0), typeof(double));
+            double numDouble = 0.0;
+
+            foreach (T num in nums)
+            {
+                numDouble = (double)Convert.ChangeType(num, typeof(double));
+                if (numDouble < minimo)
+                {
+                    minimo = numDouble;
+                }
+            }
+            return (T)Convert.ChangeType(minimo, typeof(T));
+        }
+
+        public static T Maximo<T>(this IEnumerable<T> nums)
+        {
+            double maximo = (double)Convert.ChangeType(nums.ElementAt(0), typeof(double));
+            double numDouble = 0.0;
+
+            foreach (T num in nums)
+            {
+                numDouble = (double)Convert.ChangeType(num, typeof(double));
+                if (numDouble > maximo)
+                {
+                    maximo = numDouble;
+                }
+            }
+            return (T)Convert.ChangeType(maximo, typeof(T));
+        }
+
         public static T Media<T>(this IEnumerable<T> nums)
         {
             double soma = (double)Convert.ChangeType(nums.Soma(), typeof(double));
-            //int quant = (int)nums.Count; 
-            
-            return (T)Convert.ChangeType(soma, typeof(T));
+            int quant = (int)nums.Count();
+            double media = soma / quant;
+
+            return (T)Convert.ChangeType(media, typeof(T));
         }
     }
 }
