173a73f741b1d5736dbfb1fa1512ddeac321e1c4
Raul Figueira <raulfmiranda@gmail.com>
2017-05-25 13:58:33 -0300

    Matrizes A, B e C sendo geradas com perfeição

--- exerci12/exerc12/Program.cs
+++ exerci12/exerc12/Program.cs
@@ -4,9 +4,6 @@
  * 
  * Escreva um programa que preencha uma matriz de tamanho (N, N) conforme exemplos (for N=4):
  * 
- * Matriz A:
- * i -> 4 - 4,3 - 4,3,2 - 4,3,2,1 - 3,2,1 - 2,1 - 1
- * j -> 4 - 3,4 - 2,3,4 - 1,2,3,4 - 1,2,3 - 1,2 - 1
  */
 
 using System;
@@ -29,10 +26,28 @@ namespace exerc12
             Console.WriteLine("     - Matriz A -");
             int[,] vetor = CriaVetorA(N);
             int count = 1;
-            
+
             for (int i = 0; i < N2; i++)
-            {                
-                matriz[vetor[0, i], vetor[1, i]] = count++;                
+            {
+                matriz[vetor[0, i], vetor[1, i]] = count++;
+            }
+
+            ImprimeMatriz(matriz);
+
+            // Gerando Matriz B
+            Console.WriteLine("\n\n     - Matriz B -");
+
+            matriz = MatrizA2B(matriz);
+            ImprimeMatriz(matriz);
+
+            // Gerando Matriz C
+            Console.WriteLine("\n\n     - Matriz C -");
+            vetor = CriaVetorC(N);
+            count = 1;
+
+            for (int i = 0; i < N2; i++)
+            {
+                matriz[vetor[0, i], vetor[1, i]] = count++;
             }
 
             ImprimeMatriz(matriz);
@@ -45,7 +60,7 @@ namespace exerc12
             int[,] vA = new int[2, N2];
 
             // gera linhas do vA
-            int count = 0; 
+            int count = 0;
             for (int i = N - 1; i >= 0; i--)
             {
                 for (int j = N - 1; j >= i; j--)
@@ -61,7 +76,6 @@ namespace exerc12
                 }
             }
 
-            Console.WriteLine();
             // gera colunas do vA
             count = 0;
             for (int i = N - 1; i >= 0; i--)
@@ -82,9 +96,73 @@ namespace exerc12
             return vA;
         }
 
+        public static int[,] CriaVetorC(int N)
+        {
+            int N2 = N * N;
+            int[,] vC = new int[2, N2];
+            int i = 0, j = 0, k = 0, count = 0;
+
+            while (count < N2)
+            {
+                for (; i < N - k; i++)
+                {
+                    vC[0, count] = i;
+                    vC[1, count] = j;
+                    count++;
+                    if (count == N2) return vC;
+                }
+                i--;
+                j++;
+                for (; j < N - k; j++)
+                {
+                    vC[0, count] = i;
+                    vC[1, count] = j;
+                    count++;
+                    if (count == N2) return vC;
+                }
+                i--;
+                for (j--; i >= 0 + k; i--)
+                {
+                    vC[0, count] = i;
+                    vC[1, count] = j;
+                    count++;
+                    if (count == N2) return vC;
+                }
+                j--;
+                k++;
+                for (i++; j >= 0 + k; j--)
+                {
+                    vC[0, count] = i;
+                    vC[1, count] = j;
+                    count++;
+
+                    if (count == N2) return vC;
+                }
+                i++;
+                j++;
+            }
+            return vC;
+        }
+
+        public static int[,] MatrizA2B(int[,] matrizA)
+        {
+            int N = matrizA.GetLength(0);
+            int[,] matrizB = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    matrizB[i, j] = matrizA[N - (1 + j), i];
+                }
+            }
+
+            return matrizB;
+        }
+
         public static void ImprimeMatriz(int[,] matriz)
         {
-            int N = (int)Math.Sqrt(matriz.Length); 
+            int N = (int)Math.Sqrt(matriz.Length);
 
             for (int i = 0; i < N; i++)
             {
