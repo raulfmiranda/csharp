3dcc4d2088fba74d8e9a900194cff5d1906a6b44
Raul Figueira <raulfmiranda@gmail.com>
2017-05-30 17:33:57 -0300

    Exercício 3 de XAML concluído - textbox keydown enter

---
+++ xaml-exerc3/xaml-exerc3/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ * Aluno: Raul Figueira Miranda
+ * XAML - Exercício 3  [30/05/2017]
+ * 
+ * Monte um aplicativo simples com dois TextBox, em apenas um deles 
+ * o evento KeyDown deve ser propagado para a Page, quando <Enter>.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace xaml_exerc3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter)
+            {
+                tbKeyDown.Text = "Pressionou enter!";
+            }                                  
+        }
+    }
+}
