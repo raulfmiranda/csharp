faeb9d8fd877f7ba728abd7b2885ac4600986202
Raul Figueira <raulfmiranda@gmail.com>
2017-05-30 19:21:30 -0300

    Exercício 4 de xaml concluído e 5 quase concluído

---
+++ xaml-exerc4/xaml-exerc4/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿/* 
+ * Aluno: Raul Figueira Miranda
+ * XAML - Exercício 4  [30/05/2017]
+ * 
+ * Escreva uma aplicação simples que ao pressionar 
+ * um botão incrementa um valor exibido por uma label.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace xaml_exerc4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btIncrementa_Click(object sender, RoutedEventArgs e)
+        {
+            string numStr = (string)this.tbNum.Text;
+            int num = int.Parse(numStr);
+            num++;
+            numStr = num+"";
+            this.tbNum.Text = numStr;
+        }
+    }
+}
---
+++ xaml-exerc5/xaml-exerc5/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿/* 
+ * Aluno: Raul Figueira Miranda
+ * XAML - Exercício 5  [30/05/2017]
+ * 
+ * Crie uma aplicação que adiciona dinamicamente um botão na tela, 
+ * com o título informado em um TextBox, que ao pressionado exibe 
+ * a mensagem: “Clique do botão <NomeBotão>!”.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace xaml_exerc5
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btCriarBotao_Click(object sender, RoutedEventArgs e)
+        {
+            string nomeBotao = this.tbNomeBotao.Text;
+
+            if(!nomeBotao.Equals("") && nomeBotao != null)
+            {
+                Button novoBotao = new Button();
+                novoBotao.Content = nomeBotao;
+                novoBotao.HorizontalAlignment = HorizontalAlignment.Stretch;
+                novoBotao.Click += this.btClicado;
+
+                this.stackPanel.Children.Add(novoBotao);                
+            }
+        }
+
+        private void btClicado(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button)
+            {
+                string nomeBotao = (string)(sender as Button).Content;
+                this.tbNomeBotao.Text = "Meu nome é " + nomeBotao;
+            }
+        }
+    }
+}
