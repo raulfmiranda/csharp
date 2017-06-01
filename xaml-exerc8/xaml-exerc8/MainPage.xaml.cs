dee1dec21bad9e5e907692a8ee8bc54197f7cde9
Raul Figueira <raulfmiranda@gmail.com>
2017-06-01 16:54:21 -0300

    exerc8 de xaml concluído: ListView! =D

--- xaml-exerc8/xaml-exerc8/MainPage.xaml.cs
+++ xaml-exerc8/xaml-exerc8/MainPage.xaml.cs
@@ -4,7 +4,8 @@
  * 
  * Write a program that shows ListView with columns that contain controls 
  * such as checkboxes and text boxes. The name of the columns are ID, Enabled, Value.
- * 
+ *
+ *  Fonte Pesquisa: https://www.youtube.com/watch?v=zuJZrLkKGUw 
  */
 
 using System;
@@ -21,6 +22,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,22 +33,23 @@ namespace xaml_exerc8
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Dado> Dados;
         public MainPage()
         {
             this.InitializeComponent();
 
-            List<Dado> dados = new List<Dado>();
-            dados.Add(new Dado() { Id = 0, Selecionado = false, Valor = "valor 0" });
-            dados.Add(new Dado() { Id = 1, Selecionado = true, Valor = "valor 1" });
-            dados.Add(new Dado() { Id = 2, Selecionado = false, Valor = "valor 2" });
-            dados.Add(new Dado() { Id = 3, Selecionado = true, Valor = "valor 3" });
+            Dados = new List<Dado>();
+            Dados.Add(new Dado() { Id = 0, Enabled = false, Value = "valor 0" });
+            Dados.Add(new Dado() { Id = 1, Enabled = true, Value = "valor 1" });
+            Dados.Add(new Dado() { Id = 2, Enabled = false, Value = "valor 2" });
+            Dados.Add(new Dado() { Id = 3, Enabled = true, Value = "valor 3" });            
         }
     }
 
     public class Dado
     {
         public int Id { get; set; }
-        public bool Selecionado { get; set; }
-        public string Valor { get; set; }
+        public Boolean? Enabled { get; set; }
+        public string Value { get; set; }
     }
 }
