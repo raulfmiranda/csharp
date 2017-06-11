98d6a46a9c06e7e804abb0b9744e0e5695a878c6
Raul Figueira <raulfmiranda@gmail.com>
2017-06-11 13:52:22 -0300

    exerc15 de xaml concluído: textbox e slider binding

---
+++ xaml-exerci15/xaml-exerci15/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿/* 
+ * Aluno: Raul Figueira Miranda
+ * XAML - Exercício 15 [11/06/2017]
+ * 
+ * Create an application to enter person's name and age. 
+ * Bind the person's age with a slider showing the range [1..100].
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace xaml_exerci15
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
