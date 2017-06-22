7196b4f6e3c3f2da1bac5584f2109c77a19fb21e
Raul Figueira <raulfmiranda@gmail.com>
2017-06-22 16:35:03 -0300

    Exemplo muito simples de sockets

---
+++ Sockets/Cliente/Cliente/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Cliente
+{
+	class Program
+	{
+		public static void Conectar()
+		{
+			Socket primeiroSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			IPEndPoint direcao = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+			primeiroSocket.Connect(direcao);
+			
+			Console.WriteLine("\n Conectado com êxito!");
+			Console.Write("\n Digite informação a ser enviada: ");
+			
+			string info = Console.ReadLine();
+			byte[] infoEnviar = Encoding.Default.GetBytes(info);
+			primeiroSocket.Send(infoEnviar, 0, infoEnviar.Length, 0);
+			
+			primeiroSocket.Close();			
+		}
+		
+		public static void Main(string[] args)
+		{
+			Conectar();
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("\n\n Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Sockets/Servidor/Servidor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Servidor
+{
+	class Program
+	{
+		public static void Conectar()
+		{
+			Socket primeiroSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			IPEndPoint direcao = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+			primeiroSocket.Bind(direcao);
+			primeiroSocket.Listen(5);
+			
+			Console.WriteLine("\n Escutando...");
+			
+			Socket escutar = primeiroSocket.Accept();
+			
+			Console.WriteLine("\n Conectado com êxito!");
+			
+			byte[] buffer = new byte[255];
+			int tamanho = escutar.Receive(buffer, 0, buffer.Length, 0);
+			Array.Resize(ref buffer, tamanho);
+			
+			Console.WriteLine("\n Cliente disse: " + Encoding.Default.GetString(buffer));
+			
+			primeiroSocket.Close();
+			
+		}
+		
+		public static void Main(string[] args)
+		{
+			Conectar();
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("\n\n Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
